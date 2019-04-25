8ecc6d10789e75ddf33ed18c2b59871fa4d8aefc
François Michonneau <francois.michonneau@gmail.com>
2019-04-25 06:47:04 -0400

    update docs for one_issue_per_file

--- R/one_issue_per_file.R
+++ R/one_issue_per_file.R
@@ -1,8 +1,8 @@
 ##' Opens one issue for each file found in a GitHub repository
 ##'
 ##' @title One issue per file
-##' @param owner user or organization who owns the repository
-##' @param repo name of the repository
+##' @param owner user or organization who owns the GitHub repository
+##' @param repo name of the GitHub repository
 ##' @param path subfolder to look for files
 ##' @param pattern regular expression to select the files
 ##' @param issue_title string to use for the title of the issue
@@ -12,17 +12,16 @@
 ##' @importFrom glue glue
 ##' @export
 ##' @md
-one_issue_per_file <- function(owner = "carpentries-es", repo = "python-ecology-lesson-es",
+one_issue_per_file <- function(owner, repo,
                                path = "_episodes", pattern = "md$",
                                issue_title = "Translation of {name}",
-                               issue_body = " :+1: :tada: ¡En primer lugar, gracias por tomarte el tiempo para contribuir! :tada: :+1: \n\nÉste repositorio está siendo traducido y nos gustaría mucho que puedas contribuir. Lo siguiente es un conjunto de pautas, no reglas, que hemos desarrollado para otros proyectos de traducción y nos han sido muy útiles para tener una traducción homogénea. [Convenciones](https://github.com/Carpentries-ES/board/blob/master/Convenciones_Traduccion.md). \n\n Aquí puedes comentar sobre preguntas y progreso en la tradución de este episodio. \n \n Episodio: [{path}]({html_url}).",
-                               ...) {
+                               issue_body, ...) {
 
-    files <- gh_list_files(owner = owner, repo = repo, path = path, ...)
-    files <- files[grepl(pattern, files$name), ]
+  files <- gh_list_files(owner = owner, repo = repo, path = path, ...)
+  files <- files[grepl(pattern, files$name), ]
 
-    titles <- glue::glue(issue_title, name = files$name)
-    bodies <- glue::glue(issue_body, path = files$path, html_url = files$html_url)
+  titles <- glue::glue(issue_title, name = files$name)
+  bodies <- glue::glue(issue_body, path = files$path, html_url = files$html_url)
 
   res <- purrr::walk2(titles, bodies,
                       function(x, y) {
@@ -32,4 +31,18 @@ one_issue_per_file <- function(owner = "carpentries-es", repo = "python-ecology-
                       })
 
   res
+}
+
+
+if (FALSE) {
+
+  one_issue_per_file(
+    owner = "carpentries-es",
+    repo = "python-ecology-lesson-es",
+    path = "_episodes",
+    pattern = "md$",
+    issue_title = "Translation of {name}",
+    issue_body = " :+1: :tada: ¡En primer lugar, gracias por tomarte el tiempo para contribuir! :tada: :+1: \n\nÉste repositorio está siendo traducido y nos gustaría mucho que puedas contribuir. Lo siguiente es un conjunto de pautas, no reglas, que hemos desarrollado para otros proyectos de traducción y nos han sido muy útiles para tener una traducción homogénea. [Convenciones](https://github.com/Carpentries-ES/board/blob/master/Convenciones_Traduccion.md). \n\n Aquí puedes comentar sobre preguntas y progreso en la tradución de este episodio. \n \n Episodio: [{path}]({html_url})."
+  )
+
 }
