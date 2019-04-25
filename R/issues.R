c1950d259f24e7034a84f15e2ed1ee35e7056a07
François Michonneau <francois.michonneau@gmail.com>
2019-04-25 06:06:12 -0400

    add create_milestone, update code for opening milestones

--- R/issues.R
+++ R/issues.R
@@ -1,32 +1,36 @@
-update_labels_issue_body <- "
-The lesson infrastructure committee unanimously [approved](https://github.com/carpentries/lesson-infrastructure/blob/master/2018-05-23-minutes.md#github-labels-to-use-across-our-lessons) the proposal of using the same [set of labels](https://docs.carpentries.org/topic_folders/maintainers/github_labels.html) across all our repositories during its last meeting on May 23rd, 2018.
+june_release <- "
+If your Maintainer team has decided not to participate in the [June 2019 lesson release](https://carpentries.topicbox.com/groups/maintainers/T61c75ac1a8a89538/timeline-and-process-for-june-lesson-release), please close this issue.
 
-This repository has now been converted to use the standard set of labels.
+To have this lesson included in the 18 June 2019 release, please confirm that the following items are true:
 
-If this repository used the previous set of recommended labels by Software Carpentry, they have been converted to the new one using the following rules:
+- [ ] Example code chunks run as expected
+- [ ] Challenges / exercises run as expected
+- [ ] Challenge / exercise solutions are correct
+- [ ] Call out boxes (exercises, discussions, tips, etc) render correctly
+- [ ] A schedule appears on the lesson homepage (e.g. not “00:00”)
+- [ ] Each episode includes learning objectives
+- [ ] Each episode includes questions
+- [ ] Each episode includes key points
+- [ ] Setup instructions are up-to-date, correct, clear, and complete
+- [ ] File structure is clean (e.g. delete deprecated files, insure filenames are consistent)
+- [ ] Some Instructor notes are provided
+- [ ] Lesson links work as expected
 
-| SWC legacy labels   | New 'The Carpentries' labels   |
-|--------------------|-----------------------------|
-| bug                | type:bug                    |
-| discussion         | type:discussion             |
-| enhancement        | type:enhancement            |
-| help-wanted        | help wanted                 |
-| newcomer-friendly  | good first issue            |
-| template-and-tools | type:template and tools     |
-| work-in-progress   | status:in progress          |
-
-The label `instructor-training` was removed as it is not used in the workflow of certifying new instructors anymore. The label `question` was left as is when it was in use, and removed otherwise. If your repository used custom labels (and issues were flagged with these labels), they were left as is.
-
-The lesson infrastructure committee hopes the standard set of labels will make it easier for you to manage the issues you receive on the repositories you manage.
-
-The lesson infrastructure committee will evaluate how the labels are being used in the next few months and we will solicit your feedback at this stage. In the meantime, if you have any questions or concerns, please leave a comment on [this issue](https://github.com/carpentries/lesson-infrastructure/issues/1).
-
--- The Lesson Infrastructure subcommittee
-
-PS: we will close this issue in 30 days if there is no activity.
+When all checkboxes above are completed, this lesson will be added to the 18 June lesson release. Please leave a comment on carpentries/lesson-infrastructure#26 or contact Erin Becker with questions (ebecker@carpentries.org).
 "
 
-
+##' Create issues and milestones in repositories
+##'
+##' @title Issues and milestones
+##' @param owner the owner of the GitHub repository
+##' @param repo the name of the GitHub repository
+##' @param title the title of the issue or milestone
+##' @param body the body of the issue
+##' @param ... additional parameters being passed to gh::gh
+##' @importFrom gh gh
+##' @rdname create_issue
+##' @return JSON
+##' @export
 create_issue <- function(owner, repo, title, body, ...) {
 
     gh::gh("POST /repos/:owner/:repo/issues",
@@ -34,33 +38,35 @@ create_issue <- function(owner, repo, title, body, ...) {
            body = body, ...)
 }
 
+##' @export
+##' @rdname create_issue
+create_milestone <- function(owner, repo, title, ...) {
+
+  gh::gh("POST /repos/:owner/:repo/milestones",
+    owner = owner, repo = repo, title = title, ...)
+
+}
+
 if (FALSE) {
-    repos <- readr::read_csv("/tmp/mozilla_francois0/Repositories-Grid view.csv")
+  library(tidyverse)
+  repos <- readr::read_csv("/tmp/2019-04-25-lessons.csv")
 
-    repos <- repos %>%
-        filter(`Use standard labels` == "checked") %>%
-        rename(owner = `GitHub org`,
-               repo = Repository)
+  sub_repos <- repos %>%
+    dplyr::filter(! life_cycle %in%  c("pre-alpha", "on hold"),
+      is_official) %>%
+    dplyr::rename(repo = repository)
 
-    res1 <- repos %>%
-        dplyr::slice(3:4) %>%
-        dplyr::select(repo, owner) %>%
-        purrr::pmap(function(repo, owner, ...) {
-            create_issue(owner, repo,
-                         title = "Transition to standardized GitHub labels",
-                         labels = list("type:template and tools"),
-                         body = update_labels_issue_body)
-        })
+  res1 <- sub_repos %>%
+    dplyr::slice(1:2) %>%
+    purrr::pmap(function(repo, owner, ...) {
+      create_milestone(owner, repo,
+        title = "June 2019 Release")
+    })
 
-    res2 <- repos %>%
-        slice(5:nrow(repos)) %>%
-        dplyr::select(repo, owner) %>%
-        purrr::pmap(function(repo, owner, ...) {
-            create_issue(owner, repo,
-                         title = "Transition to standardized GitHub labels",
-                         labels = list("type:template and tools"),
-                         body = update_labels_issue_body)
-        })
-
-
+  res2 <- sub_repos %>%
+    dplyr::slice(3:nrow(sub_repos)) %>%
+    purrr::pmap(function(repo, owner, ...) {
+      create_milestone(owner, repo,
+        title = "June 2019 Release")
+    })
 }
