6e3e63e52c48bd90075aa30064a433641cc6c9ac
François Michonneau <francois.michonneau@gmail.com>
2020-08-05 11:20:01 +0200

    refactor get repos

--- R/repositories.R
+++ R/repositories.R
@@ -1,33 +1,44 @@
 get_raw_repositories <- function(org, ...) {
-    i <- 1
-    tmp_res <- NULL
-    res <- vector("list", 100)
-    while (is.null(tmp_res) || tmp_res != "") {
-        tmp_res <- gh::gh("GET /orgs/:org/repos",  org = org,
-                          type = "public", page = i, ...)
-        res[[i]] <- tmp_res
-        i <- i + 1
-    }
-    unlist(res[!vapply(res, is.null, logical(1)) & nzchar(res)], recursive = FALSE)
+
+  init_res  <- gh::gh("GET /orgs/:org/repos", org = org)
+  res <- list()
+  test <- TRUE
+  i <- 1
+
+  while (test) {
+    message("Getting page: ", i, " for ", sQuote(org))
+    res <- append(res, init_res)
+
+    init_res <- tryCatch({
+      gh::gh_next(init_res)
+    },
+    error = function() {
+      test <<- FALSE
+      NULL
+    })
+    i <- i + 1
+  }
+
+  res
 }
 
 
 get_repositories <- function(org, filter = NULL) {
     raw_repo <- get_raw_repositories(org)
     res <- purrr::map_df(raw_repo, function(x) {
-        if (!grepl("archive|deprecated|ideas|hackathon|^[0-9]{4}", x[["name"]],
-                   ignore.case = TRUE) & grepl("-", x[["name"]])) {
-            list(
-                id = x[["id"]],
-                name = x[["name"]],
-                full_name = x[["full_name"]]
-            )
-        }
+      if (!grepl("archive|deprecated|ideas|hackathon|^[0-9]{4}", x[["name"]],
+        ignore.case = TRUE) & grepl("-", x[["name"]])) {
+        list(
+          id = x[["id"]],
+          name = x[["name"]],
+          full_name = x[["full_name"]]
+        )
+    }
     })
 
     if (!is.null(filter)) {
-        res <- res %>%
-            dplyr::filter(grepl(filter, name))
+      res <- res %>%
+        dplyr::filter(grepl(filter, name))
     }
 
     res
