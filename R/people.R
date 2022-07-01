c6dd337273884b5b9caae5ba2c819c8c6b0b7796
François Michonneau <francois.michonneau@gmail.com>
2022-07-01 14:46:15 +0200

    add name to query parameter

--- R/people.R
+++ R/people.R
@@ -2,7 +2,7 @@ all_people <- function() {
 
   api_key <- get_env_var("REDASH_QUERY128")
   url <- paste0(
-    "https://redash.carpentries.org/api/queries/128/results.csv?",
+    "https://redash.carpentries.org/api/queries/128/results.csv?api_key=",
     api_key
   )
 
