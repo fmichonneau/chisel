91a8304b8a740f3423aa141d80341617a6e689ec
Zhian N. Kamvar <zkamvar@gmail.com>
2022-10-28 12:29:05 -0700

    add interactive reminder to get the API key

--- R/utils.R
+++ R/utils.R
@@ -7,8 +7,21 @@ get_env_var <- function(var) {
   res <- Sys.getenv(var)
 
   if (identical(res, "")) {
-    stop(sQuote(var), " not set as environment variable.", call. = FALSE)
+    msg <- paste0(sQuote(var), " not set as environment variable.")
+    if (startsWith(var, "REDASH_QUERY") && interactive()) {
+      url <- "https://redash.carpentries.org/queries/"
+      url <- paste0(url, sub("REDASH_QUERY", "", var))
+      message(msg)
+      message(sprintf("Launching %s...\n", url))
+      browseURL(url)
+      message("Click on the kebab menu in the top left, select 'Show API Key', and copy the API Key\n(DO NOT CLICK 'REGENERATE')")
+      res <- list(askpass::askpass("Paste the API key here: "))
+      names(res) <- var
+      do.call(Sys.setenv, res)
+      res <- res[[var]]
+    } else {
+      stop(msg, call. = FALSE)
+    }
   }
-
   res
 }
