ccea67259eeaa5102e06928a98e2048b703710ae
Zhian N. Kamvar <zkamvar@gmail.com>
2022-10-28 10:05:31 -0700

    trim whitespace from API key

--- R/people.R
+++ R/people.R
@@ -31,14 +31,15 @@ all_people <- function() {
   api_key <- get_env_var("REDASH_QUERY128")
   url <- paste0(
     "https://redash.carpentries.org/api/queries/128/results.csv?api_key=",
-    api_key
+    trimws(api_key)
   )
 
-  readr::read_csv(
+  res <- readr::read_csv(
     file = url,
     show_col_types = FALSE
-  ) %>%
-  dplyr::rename(
+  ) 
+
+  dplyr::rename(res,
     email = person_email,
     pub_name =  person_name
   )
